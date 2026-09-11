8cb5af884f7ccd14baf3dffc6302af9df9678159
cruptlabs-cmd <cruptlabs@gmail.com>
2026-09-11 10:13:47 -0500

    Add realistic demo agreement dataset

---
+++ lib/demo-data.ts
@@ -0,0 +1,9 @@
+import type { AgreementInput } from "./margin";
+
+export const demoAgreements: AgreementInput[] = [
+  { id: "RM-1001", customer: "Johnson Residence", annualPrice: 219, laborHours: 2.8, laborRate: 48, materialCost: 42, otherCost: 20 },
+  { id: "RM-1002", customer: "Martinez Residence", annualPrice: 299, laborHours: 2.1, laborRate: 48, materialCost: 36, otherCost: 15 },
+  { id: "RM-1003", customer: "Pine Street Dental", annualPrice: 599, laborHours: 5.4, laborRate: 48, materialCost: 148, otherCost: 40 },
+  { id: "RM-1004", customer: "Oak Ridge Offices", annualPrice: 899, laborHours: 7.1, laborRate: 48, materialCost: 162, otherCost: 55 },
+  { id: "RM-1005", customer: "Thompson Residence", annualPrice: 249, laborHours: 1.9, laborRate: 48, materialCost: 31, otherCost: 12 }
+];
